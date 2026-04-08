820eefa52915d0f61df289090ab01e87f05742c8
PsychoShinigami <ayushrajkeshri95@gmail.com>
2026-04-08 15:28:00 +0530

    popopo

--- Story-game.py
+++ Story-game.py
@@ -1,3 +1,5 @@
 print("Welcome to the Dungeons!")
 user=input("Type your display name for the game:")
 dungeon=input("Choose a dungeoon to explore: (1) (2) (3) (4) (5) (6) (7) (8) (9) (10)")
+
+cvfgfg agueuiad
